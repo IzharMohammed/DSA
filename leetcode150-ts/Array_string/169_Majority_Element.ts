4209c62ee021a90ee296e83a4318488f17585c93
IzharMohammed <izharmohammed21@gmail.com>
2025-07-30 15:13:08 +0530

    169. Majority Element(Moore voting algo)

--- leetcode150-ts/Array_string/169_Majority_Element.ts
+++ leetcode150-ts/Array_string/169_Majority_Element.ts
@@ -1,19 +1,37 @@
 function majorityElement(nums: number[]): number {
-    const target = Math.floor(nums.length/2);
+    //Approach -1 TC:- O(nlogn)(Due to sort function) SC:- O(1) 
 
-    nums.sort((a,b)=>a-b);
-    let count = 1;
-    let i = 1;
+    // const target = Math.floor(nums.length/2);
 
-    while(count <= target){
-        if(nums[i]===nums[i-1]){
-            count++;
-            i++;
-        }else{
-            count=1;
-            i++;
+    // nums.sort((a,b)=>a-b);
+    // let count = 1;
+    // let i = 1;
+
+    // while(count <= target){
+    //     if(nums[i]===nums[i-1]){
+    //         count++;
+    //         i++;
+    //     }else{
+    //         count=1;
+    //         i++;
+    //     }
+    // }
+
+    // return nums[i-1];
+
+    //Approach - 2 TC:- O(n) SC:- O(1)
+    //Boyer-Moore Voting Algorithm
+
+    let count = 0;
+    let candidate: number | null = null;
+
+    for (const num of nums) {
+        if (count === 0) {
+            candidate = num;
         }
+        count += (num === candidate) ? 1 : -1;
     }
 
-    return nums[i-1];
+    return candidate!;
+
 };
